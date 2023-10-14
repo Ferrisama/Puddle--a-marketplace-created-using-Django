818ccce7f70bdd3fd5fc7f6eb670d88cd52711e3
Ferrisama <asmitghosh3@gmail.com>
2023-10-15 00:24:28 +0530

    changes fifeteenth

--- item/forms.py
+++ item/forms.py
@@ -9,3 +9,10 @@ class NewItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ('category', 'name', 'description', 'price', 'image',)
+        widgets = {
+            'category': forms.Select(attrs={'class': INPUT_CLASSES}),
+            'name': forms.TextInput(attrs={'class': INPUT_CLASSES}),
+            'description': forms.Textarea(attrs={'class': INPUT_CLASSES}),
+            'price': forms.TextInput(attrs={'class': INPUT_CLASSES}),
+            'image': forms.FileInput(attrs={'class': INPUT_CLASSES})
+        }
--- item/views.py
+++ item/views.py
@@ -20,6 +20,18 @@ def detail(request, pk):
 
 @login_required
 def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = NewItemForm()
+
     form = NewItemForm()
 
     return render(request, 'item/form.html',
